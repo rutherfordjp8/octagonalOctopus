b494cc740e8a687325b582c579a1221514e49d42
Brian van den Broek <vanden@gmail.com>
2017-08-08 16:31:53 -0700

    Start on App state

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -8,6 +8,18 @@ class App extends React.Component {
     super(props);
     this.state = { 
 
+      // State like things that don't exist on the app's first render,
+      // but depend on a a Game having been created, joined by
+      // players, and started.
+      players: [],
+      role: undefined,
+      otherCharInfo = {},
+
+      // Another worry: do games with many players have more rounds?
+      // In which case, roundsHistory isn't the right thing.
+      roundsHistory: [null, null, null, null, null],
+
+      missionParticipation = [],
     }
   }
 
