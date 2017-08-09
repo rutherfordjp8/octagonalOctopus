0c86867f9a44e3f43fb0eac4bbf2cca7f789e603
Brian van den Broek <vanden@gmail.com>
2017-08-09 12:14:00 -0700

    Fix props in PickMissionPlayers

--- react-client/src/components/PickMissionPlayers.jsx
+++ react-client/src/components/PickMissionPlayers.jsx
@@ -5,14 +5,9 @@ class PickMissonPlayer extends React.Component {
   constructor(props) {
     super(props);
 
-    this.missionCount = props.missionCount;
   }
 
   render() {
-
-    players = []// Right array from props, but also rendered nicely as
-                // a string with the Oxford coma if more than two
-                // others (can there be missions of size 4?)
 
     return (
       <div>
@@ -23,7 +18,7 @@ class PickMissonPlayer extends React.Component {
 
         <h3> Pick Mission Players </h3>
 
-        Pick {this.missionCount} players to go:
+        Pick {this.props.missionCount} players to go:
 
         Appropriate Widget here
 
