a4e4de8b18dcb608b3ff16a06aaf7fb7f921c559
Brian van den Broek <vanden@gmail.com>
2017-08-08 19:43:00 -0700

    Add SubmitButton to merlinChoice

--- react-client/src/components/MerlinChoice.jsx
+++ react-client/src/components/MerlinChoice.jsx
@@ -19,6 +19,9 @@ class MerlinChoice extends React.Component {
         <h3> Choose Merlin </h3>
         EnterMerlinChoice Not obvious if it should be a component or
         just in-lined, here
+
+        <SubmitButton />
+
         (Shown only to the Assassin)
       </div>
       )
