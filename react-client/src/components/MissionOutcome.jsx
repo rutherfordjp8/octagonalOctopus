9c4204c963981a35f00b2dec53b31e5189d1af7a
Brian van den Broek <vanden@gmail.com>
2017-08-08 19:30:57 -0700

    Add basic Props to MissionOutcome

--- react-client/src/components/MissionOutcome.jsx
+++ react-client/src/components/MissionOutcome.jsx
@@ -5,19 +5,32 @@ class MissionOutcome extends React.Component {
   constructor(props) {
     super(props);
 
-  }
+    this.failVotes = props.failVotes;
+    this.successVotes = props.successVotes;
+    }
+
+  clickHandler() {
+    // Do click stuff. Presently, unclear 1) what to send to server and
+    // 2) if the function can live here and be called from within the
+    // render method or if it needs to be bound at app level and messages
+    // sent from there.
+    }
 
   render() {
-
 
     return (
       <div>
 
         <h5> Mission Outcome  </h5>
 
-         <Stats /> <Timer />
 
-         <NextButton />
+         <Stats /> <Timer seconds={30}/>
+
+        Fail votes: {this.failVotes}
+
+        Success votes: {this.successVotes}
+
+         <NextButton clickHandler={this.clickHandler()}/>
          // Unclear from discussion if Buttons should be components
       </div>
       )
