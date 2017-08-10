58088c2317ba3b3a615db9422ce38f8bf1da4c80
Brian van den Broek <vanden@gmail.com>
2017-08-10 11:53:19 -0700

    MOAR

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -8,15 +8,34 @@ class App extends React.Component {
     super(props);
     this.state = {
 
-      pageID: 'PlayerWaitingForPlayersScreen',
+      // pageID: 'PlayerWaitingForPlayersScreen'
+
+      
+
+      // pageID: // DiscussMissionPlayersScreen
+      // pageID: // EnterMissionPlayersScreen
+      // pageID: // GameOutcomeScreen
+
+      // pageID: // MerlinChoiceScreen
+      // pageID: // MissionOutcomeScreen
+      // pageID: // MissionVoteScreen
+      
+      
+      pageID: 'AwaitAssassinScreen',
+      //pageID: 'AwaitMissionOutcomeScreen', //works
+      //pageID: 'PlayerWaitingForPlayersScreen', //works
+      //pageID: 'PlayerEnternameScreen', // Works
+      //pageID: 'GameOwnerWaitingForPlayersScreen', // Works
+      //pageID: 'WelcomeScreen', // Works
+      //pageID: 'GameOwnerEnterNameScreen',  // Works
 
       // State like things that don't exist on the app's first render,
       // but depend on a a Game having been created, joined by
       // players, and started.
       players: [],
-      role: undefined,
+      role: 'Merlin',
       otherCharInfo: {},
-      spyCount: 0,
+      spyCount: 3,
 
       roundsHistory: [null, null, null, null, null],
 
@@ -30,34 +49,33 @@ class App extends React.Component {
   // then simply call the function that is the value of the one object
   // with the props of the other.
     this.screenDispatch = {
-      AwaitAssassinScreen: function(propsObj) {return '<AwaitAssassin spyCount={propsObj.spyCount} />'},
-      // AwaitMissionOutcomeScreen: (propsObj) => {},
-      // DiscussMissionPlayersScreen: (propsObj) => {},
-      // EnterMissionPlayersScreen: (propsObj) => {},
-      // GameOutcomeScreen: (propsObj) => {},
-      GameOwnerEnterNameScreen: function(pObj) { return 'GameOwnerEntername' + pObj['thing'] },
-      GameOwnerWaitingForPlayersScreen: function(pObj) { return 'GameOwnerWaitPlayers' + pObj['code'] },
-      // MerlinChoiceScreen: (propsObj) => {},
-      // MissionOutcomeScreen: (propsObj) => {},
-      // MissionVoteScreen: (propsObj) => {},
-      PlayerEnternameScreen: function(pObj) { return 'PlayerEntername' + pObj['thing'] },
-      PlayerWaitingForPlayersScreen: function(pObj) { return 'PlayerWaitForPlayers' + pObj['button'] },
-//      WelcomeScreen: function(pObj) { return "WELCOME" + pObj },
+       AwaitAssassinScreen: function(pObj) {return 'AwaitAssassin +spyCount=' + pObj.spyCount },
+       AwaitMissionOutcomeScreen: function(pObj) {return 'AwaitMissionOutcome' + pObj.role + pObj.history },
+
+//       // DiscussMissionPlayersScreen: (propsObj) => {},
+//       // EnterMissionPlayersScreen: (propsObj) => {},
+//       // GameOutcomeScreen: (propsObj) => {},
+       GameOwnerEnterNameScreen: function(pObj) { return 'GameOwnerEntername' + pObj['thing'] },
+       GameOwnerWaitingForPlayersScreen: function(pObj) { return 'GameOwnerWaitPlayers' + pObj['code'] },
+//       // MerlinChoiceScreen: (propsObj) => {},
+//       // MissionOutcomeScreen: (propsObj) => {},
+//       // MissionVoteScreen: (propsObj) => {},
+       PlayerEnternameScreen: function(pObj) { return 'PlayerEntername' + pObj['thing'] },
+       PlayerWaitingForPlayersScreen: function(pObj) { return 'PlayerWaitForPlayers' + pObj['button'] },
       WelcomeScreen: function(pObj) { return 'WELCOME' + pObj['thing'] },
     }
 
-
     this.propsDispatch = {
       'AwaitAssassinScreen': {spyCount:this.state.spyCount},
-      // AwaitMissionOutcomeScreen:
-      // DiscussMissionPlayersScreen:
-      // EnterMissionPlayersScreen:
-      // GameOutcomeScreen:
+      'AwaitMissionOutcomeScreen': {role:this.state.role, history:this.state.roundsHistory },
+      // // DiscussMissionPlayersScreen:
+      // // EnterMissionPlayersScreen:
+      // // GameOutcomeScreen:
       'GameOwnerEnterNameScreen': {thing:'Stuff'},
       'GameOwnerWaitingForPlayersScreen': {code: 'kdjfkjfd'},
-      // MerlinChoiceScreen:
-      // MissionOutcomeScreen:
-      // MissionVoteScreen:
+      // // MerlinChoiceScreen:
+      // // MissionOutcomeScreen:
+      // // MissionVoteScreen:
       'PlayerEnternameScreen':  {thing:'Fruit'},
       'PlayerWaitingForPlayersScreen': {button: 'rrr'},
       'WelcomeScreen': {thing:'People'}
