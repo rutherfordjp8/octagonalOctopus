73cb33af9c4c47bb9c79f9e9af6c9f725c5bd03d
Yang Lu <me@yang.lu>
2017-08-14 11:02:37 -0700

    first draft database query functions

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -13,22 +13,83 @@ db.once('open', function() {
 });
 
 var games = mongoose.Schema({
-  token: String,
-  players: String,
-  results: String  
+  token: {type: String, required: true},	
+  playerRoles: String,
+  results: String, 
+  playerIds: String,
 });
 
-var Game = mongoose.model('Game', games);
+module.exports.Game = mongoose.model('Game', games);
 
-var selectGame = function(token, callback) {
-  Game.find({token});
+module.exports.selectGame = function(token, callback) {
+  Game.find({token}, (err, game) => {
+  	console.log(game);
+  	callback(game);
+  });
 };
 
-var createGame = function(token, players, results, callback) {
+module.exports.createGame = function(token, hostName, clientId, callback) {
+	Game.create({token}, (err) => {
+		if (err) {
+			callback(err);
+		} else {
+			var playerRoles = {};
+			playerIds[hostName] = clientId;
+			var playerIds = JSON.stringify(playerIds);
+			var results = [];
+			Game.update({token}, {playerIds, results}, (err) => {
+				callback(err);
+			});
+		}
+	});
 };
 
-var updateResults = function (results, callback) {
+module.exports.addPlayer = function(token, playerName, clientId, callback) {
+	Game.find({token}, (err, game) => {
+		var playerIds = JSON.parse(game.playerIds);
+		playerIds[playerName] = clientId;
+		playerIds = JSON.stringify(playerIds);
+		Game.update({token}, {playerIds}, (err) => {
+			callback(err);
+		};
+	});
 };
 
+module.exports.removePlayer = function(token, playerName, callback) {
+	Game.find({token}, (err, game) => {
+		var playerIds = JSON.parse(game.playerIds);
+		delete playerIds[playerName];
+		playerIds = JSON.stringify(playerIds);
+		Game.update({token}, {playerIds}, (err) => {
+			callback(err);
+		};
+	});
+};
 
-module.exports.selectGame = selectGame;
+module.exports.addRoles = function(token, playerRoles, callback) {
+	var playerRoles = JSON.stringify(playerRoles);
+	Game.update({token}, {playerRoles}, (err) => {
+		callback(err);
+	});
+};
+
+module.exports.updateResults = function (roundResults, callback) {
+	Game.find({token}, (err, game) => {
+		var results = JSON.parse(game.results);
+		results.push(roundResults);
+		results = JSON.stringify(results);
+		Game.update({token}, {results}, (err) => {
+			callback(err);
+		})
+	});
+};
+
+// module.exports.getMerlin = function(token, callback) {
+// 	Game.find({token}, (err, game) => {
+
+// 	})
+// }
+
+// module.exports.getPlayerRoleMapping = function(token, callback) {
+	
+// }
